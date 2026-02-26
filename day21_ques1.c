028cf7fd62853829943ba05ec28019dd9200874f
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-02-26 22:40:22 +0530

    Add linked list implementation in day21_ques1.c
    Implement linked list creation and traversal.

---
+++ day21_ques1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        // Allocate memory for new node
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        // If list is empty
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Traverse and print the linked list
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
