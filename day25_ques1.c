1c43eee3b85b8a16d603d98ac91bfa4ee52fed1f
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-02-28 23:01:34 +0530

    Create day25_ques1.c

---
+++ day25_ques1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count occurrences of key
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, value, key;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Read number of nodes
+    scanf("%d", &n);
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Read key
+    scanf("%d", &key);
+
+    // Count occurrences
+    int result = countOccurrences(head, key);
+
+    // Print result
+    printf("%d\n", result);
+
+    return 0;
+}
