918f658bc8fec10c09dc3e24bf204c430d16258b
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-03-17 20:04:53 +0530

    Create day45_ques2.c

---
+++ day45_ques2.c
@@ -0,0 +1,9 @@
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    
+    return 1 + (left > right ? left : right);
+}
