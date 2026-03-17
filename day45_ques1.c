a2485071ad36fd62281941dde6c69f717c115216
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-03-17 20:00:05 +0530

    Create day45_ques1.c

---
+++ day45_ques1.c
@@ -0,0 +1,17 @@
+Problem Statement:
+Find the height (maximum depth) of a given binary tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+
+Output Format:
+- Print the height of the tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 -1
+
+Output:
+3
