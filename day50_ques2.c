8077aee577aaaefe8ec81f211b4e06fcd4e9b063
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-03-26 22:37:54 +0530

    Create day50_ques2.c

---
+++ day50_ques2.c
@@ -0,0 +1,11 @@
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    while (root != NULL) {
+        if (root->val == val)
+            return root;
+        else if (val < root->val)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return NULL;
+}
