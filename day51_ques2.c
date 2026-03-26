b1e3d4d254610eecaef19a2feb87689a8932e36e
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-03-26 22:43:21 +0530

    Create day51_ques2.c

---
+++ day51_ques2.c
@@ -0,0 +1,14 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root != NULL) {
+        if (p->val < root->val && q->val < root->val) {
+            root = root->left;
+        } 
+        else if (p->val > root->val && q->val > root->val) {
+            root = root->right;
+        } 
+        else {
+            return root;
+        }
+    }
+    return NULL;
+}
