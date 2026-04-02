681fc2b01ff854b4536e90461d541df1c437708b
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-04-02 20:58:30 +0530

    Add function to find number of provinces
    Implement depth-first search to count provinces.

---
+++ day61_ques2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void dfs(int** isConnected, int* visited, int n, int city) {
+    visited[city] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (isConnected[city][i] == 1 && !visited[i]) {
+            dfs(isConnected, visited, n, i);
+        }
+    }
+}
+
+int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
+    int n = isConnectedSize;
+    int visited[n];
+
+    
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    int provinces = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(isConnected, visited, n, i);
+            provinces++;  
+        }
+    }
+
+    return provinces;
+}
