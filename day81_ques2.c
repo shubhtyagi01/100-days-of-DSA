d276754fc81c56f5801b4e99dd56832bd53994b7
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-04-23 21:48:46 +0530

    Implement binary search function

---
+++ day81_ques2.c
@@ -0,0 +1,13 @@
+int search(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+
+    return -1;
+}
