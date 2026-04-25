3f0b2da11138e2ba6553218ce285114304c97177
shubhtyagi01 <shubhtyagi8004@gmail.com>
2026-04-25 23:08:01 +0530

    Implement function to find peak element in array

---
+++ day84_ques2.c
@@ -0,0 +1,14 @@
+int findPeakElement(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while(left < right) {
+        int mid = left + (right - left) / 2;
+
+        if(nums[mid] > nums[mid + 1])
+            right = mid;
+        else
+            left = mid + 1;
+    }
+
+    return left;
+}
